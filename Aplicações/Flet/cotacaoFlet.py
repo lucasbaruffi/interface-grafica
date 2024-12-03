a07c84a4648def75269428453830ba092642ea78
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-12-03 09:39:38 -0300

    não é facil

--- Aplicações/Flet/cotacaoFlet.py
+++ Aplicações/Flet/cotacaoFlet.py
@@ -3,12 +3,35 @@ import flet as ft
 def main(page: ft.Page):
 
     page.fonts = {
-        "Montserrat Regular": "/Flet/fonts/MontserratAlternates-Regular.otf"
+        "Montserrat": "https://github.com/JulietaUla/Montserrat/blob/master/fonts/ttf/Montserrat-Regular.ttf"
     }
 
-    texto_base = ft.Text("Cotação de Moedas", font_family="Montserrat Regular")
-
+    texto_base = ft.Text(
+        "Cotação de Moedas", 
+        font_family="Montserrat",
+        size=42,
+        weight=ft.FontWeight.W_100
+        )
+    
     page.add(ft.Row([texto_base]))
+
+
+    escolha_moeda = ft.Text(
+    "Escolha a Moeda:", 
+    font_family="Montserrat",
+    size=31,
+    weight=ft.FontWeight.W_100
+    )
+    
+    selecao = ft.Dropdown(
+        value="Opções", color="White",
+        options=[
+            ft.dropdown.Option("Op1"),
+            ft.dropdown.Option("Op2"),
+            ft.dropdown.Option("Op3")])
+
+    page.add(ft.Row([escolha_moeda, selecao]))
+
 
     pass
 
