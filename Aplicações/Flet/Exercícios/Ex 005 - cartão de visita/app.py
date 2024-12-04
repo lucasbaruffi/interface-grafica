117d8374ca2d8d7992b3eb8c455ded7a4deacfe3
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-12-04 16:08:57 -0300

    éh, ok

--- Aplicações/Flet/Exercícios/Ex 005 - cartão de visita/app.py
+++ Aplicações/Flet/Exercícios/Ex 005 - cartão de visita/app.py
@@ -2,39 +2,63 @@ import flet as ft
 
 def main(page: ft.Page):
 
-    foto = ft.Image("Exercícios\\Ex 005 - cartão de visita\\avatar.png")
+    page.fonts = {
+        "Inter": "Exercícios\\Ex 005 - cartão de visita\\Inter.ttc"
+    }
+
+    foto = ft.Image("Exercícios\\Ex 005 - cartão de visita\\assets\\avatar.png")
 
     nome = ft.Text(
-        value="Nome Sobrenome"
+        value = "Nome Sobrenome",
+        font_family = "Inter Bold",
+        color = "#333333",
+        weight = "bold",
+        size = 18
     )
 
     descricao = ft.Text(
-        value="Descrição sobre o contato"
+        value="breve descrição",
+        font_family= "Inter",
+        color = "#666666",
+        italic = True,
+        size = 14
     )
-
     btnSeguir = ft.Button(
-        text="Seguir"
+        content = ft.Text(
+            value="Seguir",
+            font_family="Inter",
+            weight="bold"
+        ),
+        width = 187,
+        height = 40,
+        bgcolor = "#007BFF",
+        color = "#FFFFFF",
+        style = ft.ButtonStyle(
+            shape = ft.RoundedRectangleBorder(radius=5),
+        
+        )
     )
 
     colunaPrincipal = ft.Column(
         [foto,
          nome,
          descricao,
-         btnSeguir]
+         btnSeguir],
+         spacing = 30,
+         horizontal_alignment = ft.CrossAxisAlignment.CENTER
     )
 
     page.add(
         ft.Container(
             colunaPrincipal,
-            bgcolor="#F0F0F0",
-            width=300,
-            height=375,
-            border_radius=10
-
+            bgcolor = "#F0F0F0",
+            width = 300,
+            height = 345,
+            border_radius = 10,
+            alignment = ft.alignment.center,
+            padding = 48
         )
     )
-
-
 
 
 
