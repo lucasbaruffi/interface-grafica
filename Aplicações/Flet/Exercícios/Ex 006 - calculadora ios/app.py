c914759e56cc5130af5aeb6745365a0019f4bfbd
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-12-05 08:45:38 -0300

    Update app.py

--- Aplicações/Flet/Exercícios/Ex 006 - calculadora ios/app.py
+++ Aplicações/Flet/Exercícios/Ex 006 - calculadora ios/app.py
@@ -19,10 +19,25 @@ def main(page:ft.Page):
             )
         )
         
-        
+    textPercent = ft.Text(
+        value="%",
+        size=25,
+        text_align=ft.TextAlign.CENTER,
+        color="#000000"
+    )
+
+    btnPercentC = ft.TextButton(
+        content=textPercent,
+        width=80,
+        height=80,
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(100),
+            bgcolor="#A6A6A6"
+            )
+        )       
 
 
-    page.add(btnAC)
+    page.add(btnAC,btnPercentC)
 
 
 ft.app(target=main)
